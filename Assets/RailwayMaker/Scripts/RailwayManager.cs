df6092cf5c676297297d561cc7e0aaa5541eb42e
Lewis B <lewis007bonner@hotmail.co.uk>
2022-11-30 14:07:56 +0000

    Initial

---
+++ Assets/RailwayMaker/Scripts/RailwayManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MrWhimble.RailwayMaker
+{
+    public class RailwayManager : MonoBehaviour
+    {
+
+    }
+}
