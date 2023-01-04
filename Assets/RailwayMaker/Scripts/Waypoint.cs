a6b850dce665a479b499f444cd03a7a974c3b807
Lewis Bonner <lewis007bonner@hotmail.co.uk>
2023-01-04 00:37:16 +0000

    updated waypoints

--- Assets/RailwayMaker/Scripts/Waypoint.cs
+++ Assets/RailwayMaker/Scripts/Waypoint.cs
@@ -2,11 +2,37 @@
 
 namespace MrWhimble.RailwayMaker
 {
-    public class Waypoint : MonoBehaviour
+    public class Waypoint : MonoBehaviour, IWaypoint
     {
-        public int curve;
-        public float t;
+        private int _curveIndex;
+
+        public int CurveIndex
+        {
+            get => _curveIndex;
+            set { }
+        }
         
+        private float _ratioAlongCurve;
+        public float RatioAlongCurve
+        {
+            get => _ratioAlongCurve;
+            set => _ratioAlongCurve = Mathf.Clamp01(value);
+        }
         
+    }
+
+    public interface IWaypoint
+    {
+        int CurveIndex
+        {
+            get;
+            set;
+        }
+
+        float RatioAlongCurve
+        {
+            get;
+            set;
+        }
     }
 }
